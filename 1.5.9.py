1b40871a918e6b74e5364a1a2a710eeb1fe895b7
Anatoly <epo_android@mail.ru>
2018-03-02 19:18:36 +0300

    add 1.5.9

---
+++ 1.5.9.py
@@ -0,0 +1,23 @@
+class Buffer:
+    lst=[]
+    def __init__(self):
+        self.lst=[]
+
+    def add(self, *a):
+        self.lst+=[*a]
+        while len(self.lst) > 4:
+            print(sum(self.lst[:5]))
+            self.lst=self.lst[5:]
+
+    def get_current_part(self):
+        return self.lst
+
+buf = Buffer()
+buf.add(1, 2, 3)
+buf.get_current_part() # вернуть [1, 2, 3]
+buf.add(4, 5, 6) # print(15) – вывод суммы первой пятерки элементов
+buf.get_current_part() # вернуть [6]
+buf.add(7, 8, 9, 10) # print(40) – вывод суммы второй пятерки элементов
+buf.get_current_part() # вернуть []
+buf.add(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1) # print(5), print(5) – вывод сумм третьей и четвертой пятерки
+buf.get_current_part() # вернуть [1]
