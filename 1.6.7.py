c3b1b90ef3a1cd6484f19ad696b0dd69d000ee99
Anatoly <epo_android@mail.ru>
2018-03-02 21:03:57 +0300

    done 1.6.7

--- 1.6.7.py
+++ 1.6.7.py
@@ -1,27 +1,23 @@
-cl = {}
+def fnd(classes, parrent, child):
+    if parrent == child:
+        return "Yes"
+    elif child not in classes:
+        return "No"
+    elif parrent in classes[child]:
+        return "Yes"
+    else:
+        for p in classes[child]:
+            if fnd(classes,parrent,p)=="Yes":
+                return "Yes"
+    return "No"
 
+cl = {}
 for i in range(int(input())):
     s = input().split(" : ", 1)
     if len(s) == 2:
-        cl[s[0]] = set(s[1].split())
+        cl[s[0]] = s[1].split()
     else:
-        cl[s[0]] = {}
-print(cl)
-res=[]
+        cl[s[0]] = []
 for i in range(int(input())):
-    tmp = "No"
     s = input().split()
-    if s[0] == s[1]:
-        tmp = "Yes"
-    elif s[1] not in cl:
-        tmp = "No"
-    elif s[0] in cl[s[1]]:
-        tmp = "Yes"
-    else:
-        for cls in cl[s[1]]:
-            if s[0] in cl[cls]:
-                tmp = "Yes"
-                break
-    print(tmp)
-    res.append(tmp)
-print(res)
+    print(fnd(cl,s[0],s[1]))
