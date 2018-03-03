701c492926b873b201747dd6ba8cf76b377cb78e
Anatoly <epo_android@mail.ru>
2018-03-03 09:43:06 +0300

    done 2.1.7

--- 2.1.7.py
+++ 2.1.7.py
@@ -1,23 +1,31 @@
-def addsons(ex, obj):
-    if ex in obj:
-        useders.add(s)
-    for e in obj:
-        addsons(ex, e)
-
+def fnd(classes, parrent, child):
+    if parrent == child:
+        return True
+    elif child not in classes:
+        return False
+    elif parrent in classes[child]:
+        return True
+    else:
+        for p in classes[child]:
+            if fnd(classes,parrent,p):
+                return True
+    return False
 
 ers = {}
-useders = set()
+useders = []
 for i in range(int(input())):
     s = input().split(" : ", 1)
     if len(s) == 2:
         ers[s[0]] = s[1].split()
     else:
         ers[s[0]] = []
-print(ers)
+
 for i in range(int(input())):
     s = input()
     if s in useders:
         print(s)
-    useders.add(s)
-    addsons(s, ers)
-    print(useders)
+    else:
+        useders.append(s)
+        for ex in ers:
+            if fnd(ers,s,ex):
+                useders.append(ex)
