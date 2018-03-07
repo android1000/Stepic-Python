7dc3ed4fc968633907fc2c45d087434355364b9d
Anatoly <epo_android@mail.ru>
2018-03-07 13:25:39 +0300

    add 2.3.5

---
+++ 2.3.5.py
@@ -0,0 +1,18 @@
+import itertools
+
+
+def primes():
+    x = 2
+    while True:
+        count = 0
+        for i in range(2, x+1):
+            if x % i == 0:
+                count+=1
+        if count == 1:
+            yield x
+        x += 1
+
+
+print(list(itertools.takewhile(lambda x: x <= 40, primes())))
+# [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31]
+# primes()
