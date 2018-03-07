d4abc3ead4cb1cd84de7ef52b8b4c8ab976a42cc
Anatoly <epo_android@mail.ru>
2018-03-07 16:04:53 +0300

    redone 2.4.4 and 2.4.6

--- 2.4.4.py
+++ 2.4.4.py
@@ -2,10 +2,6 @@ path1="1.txt"
 path2="2.txt"
 with open(path1,"r") as f1:
     with open(path2,"w") as f2:
-        res=[]
-        for line in f1:
-            res+=[line.strip("\n")+"\n"]
-        i=len(res)
-        while i>0:
-            f2.write(res[i-1])
-            i-=1
+        res=f1.read().splitlines()
+        res.reverse()
+        f2.write("\n".join(res))
--- 2.4.6.py
+++ 2.4.6.py
@@ -1,10 +1,7 @@
 import os.path
 
-print(os.getcwd())
-print(os.listdir("main"))
-
 for cur_dir, dirs, files in os.walk("main"):
     for file in files:
-        if file[len(file)-3:]==".py":
+        if file.endswith(".py"):
             print(cur_dir)
             break
