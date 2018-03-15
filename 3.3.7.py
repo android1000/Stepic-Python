d039aac915c6f6603cb2636478da8129876b9983
Anatoly <epo_android@mail.ru>
2018-03-15 11:10:23 +0300

    done 3.3.7

--- 3.3.7.py
+++ 3.3.7.py
@@ -1,8 +1,11 @@
 import requests
 import re
 
-
-with open("1.html","r") as f:
-    links=re.findall('<a.*href="([^"]*)"',f.read())
-links.sort
-print(links)
+links=[]
+res=requests.get(input().strip())
+if res.status_code==200:
+    for link in re.findall(r'(?:<a.*href=[\"\']+)(?!\.\.)(?:\w*:\/+)?([^\/\'\":]+)',res.text):
+        if link not in links and link !='':
+            links+=[link]
+links.sort()
+print("\n".join(links))
